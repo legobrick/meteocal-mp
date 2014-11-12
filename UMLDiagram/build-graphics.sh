9f14faaeca442b6169cbb9b76610b130011a8d8b
Paolo Polidori <polyp91@gmail.com>
2014-11-12 16:55:38 +0100

    sequence diagram modificato registrazione utente + ultimato comando build graphics

--- UMLDiagram/build-graphics.sh
+++ UMLDiagram/build-graphics.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-for i in $(find . -name "*.asta"); do
-    /usr/lib/astah_professional/astah-command.sh -f "${i}" -image -t png -o ${i%/*}
-done
+list=($(find . -name "*.asta"))
 IFS=$AVEIFS
+for i in "${!list[@]}"; do
+    /usr/lib/astah_professional/astah-command.sh -f "${list[$i]}" -image -t png -o ${list[$i]%/*}
+    a=${list[$i]##*/}
+    pct=$(bc <<< "scale=2; $i*100/${#list[@]}")
+    printf "%-30s  %-30s \n" "Image for ${a%.*} created!" "[ $pct% ]"
+done
 exit 0
