d699b2dfadbc6c86eda99eb2ca36604f998dc9c7
Paolo Polidori <polyp91@gmail.com>
2014-11-14 12:37:16 +0100

    ops, dimenticato script

--- UMLDiagram/build-graphics.sh
+++ UMLDiagram/build-graphics.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+if [ -s "/Applicazioni/astah professional/astah-command.sh" ]; then
+    acmd="/Applicazioni/astah\ professional/astah-command.sh"
+else
+    acmd="/usr/lib/astah_professional/astah-command.sh"
+fi
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 list=($(find . -name "*.asta"))
 IFS=$AVEIFS
 for i in "${!list[@]}"; do
-    /usr/lib/astah_professional/astah-command.sh -f "${list[$i]}" -image -t png -o ${list[$i]%/*}
+    $($acmd -f "${list[$i]}" -image -t png -o ${list[$i]%/*})
     a=${list[$i]##*/}
     pct=$(echo $(bc <<< "scale=2; ($i+1)*100/${#list[@]}") | sed -r "/[0-9]+\.[0-9]+/s/\./,/g")
     printf "%-70s  %s%6.2f%s \n" "Image for ${a%.*} created!" "[ " "$pct" "% ]"
