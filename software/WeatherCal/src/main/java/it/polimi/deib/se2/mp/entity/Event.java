813e625cd8043791d8f9a247a5acb510a8144704
Paolo Polidori <polyp91@gmail.com>
2014-12-17 11:55:36 +0100

    Aggiunta entity Event (vuota) e fatto setup postgresql + istruzioni

---
+++ software/WeatherCal/src/main/java/it/polimi/deib/se2/mp/entity/Event.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.deib.se2.mp.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author paolo
+ */
+@Entity
+public class Event implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Event)) {
+            return false;
+        }
+        Event other = (Event) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.polimi.deib.se2.mp.entity.Event[ id=" + id + " ]";
+    }
+    
+}
---
+++ software/installation.md
@@ -0,0 +1,28 @@
+WeatherCal Platorm installation guide
+=======
+
+# Required software
+* [Java SE Development Kit 8](http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html). If you want to use Java 7, switch the maven.compiler.source and maven.compiler.target version in your pom.xml from 1.8 to 1.7.
+* [Glassfish Open Source Edition 4.1](https://glassfish.java.net/download.html)
+* [PostgreSQL >= 9.1](http://www.postgresql.org/download/)
+
+
+# PostgreSQL & DB Setup
+* Set up postgresql for accepting Unix-socket and TCP/IP connections from localhost without password for the user postgres, creating it if does not exists
+* Enstablish a connection to the DBMS (psql, pgAdmin, *DBC && coding, ...) and issue the next query:
+
+```
+DROP DATABASE IF EXISTS weathercal;
+
+CREATE DATABASE weathercal
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'it_IT.UTF-8'
+       LC_CTYPE = 'it_IT.UTF-8'
+       CONNECTION LIMIT = 100;
+
+CREATE SCHEMA development
+  AUTHORIZATION postgres;
+
+```
