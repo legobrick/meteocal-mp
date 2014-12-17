8a19b1014dd635968cc9fb45b3fbde152a2f6ade
Marco Edemanti <edemantimarco@gmail.com>
2014-12-17 17:07:00 +0100

    loginview, primefaces, layout minimo

---
+++ software/WeatherCal/target/generated-sources/annotations/it/polimi/deib/se2/mp/entity/Event_.java
@@ -0,0 +1,13 @@
+package it.polimi.deib.se2.mp.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-17T17:02:56")
+@StaticMetamodel(Event.class)
+public class Event_ { 
+
+    public static volatile SingularAttribute<Event, Long> id;
+
+}
