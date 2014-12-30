3c8c32dd5148d12563f17d06cb36f7f9471d580a
Marco Edemanti <edemantimarco@gmail.com>
2014-12-30 10:33:52 +0100

    aggiunta una prova della searched user page

--- software/WeatherCal/target/generated-sources/annotations/it/polimi/deib/se2/mp/weathercal/entity/Owner_.java
+++ software/WeatherCal/target/generated-sources/annotations/it/polimi/deib/se2/mp/weathercal/entity/Owner_.java
@@ -8,7 +8,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+<<<<<<< Updated upstream
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-27T23:03:05")
+=======
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-25T20:51:50")
+>>>>>>> Stashed changes
 @StaticMetamodel(Owner.class)
 public class Owner_ { 
 
