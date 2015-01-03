b584fb5cf98198d0cd3673e57b7583c2f1b5339f
Paolo Polidori <polyp91@gmail.com>
2015-01-04 00:50:11 +0100

    Creato package utils e spostato LoggerProducer al suo interno

--- software/WeatherCal/src/main/java/it/polimi/deib/se2/mp/weathercal/util/LoggerProducer.java
+++ software/WeatherCal/src/main/java/it/polimi/deib/se2/mp/weathercal/util/LoggerProducer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package it.polimi.deib.se2.mp.weathercal;
+package it.polimi.deib.se2.mp.weathercal.util;
 
 import java.util.logging.Logger;
 import javax.enterprise.inject.Produces;
