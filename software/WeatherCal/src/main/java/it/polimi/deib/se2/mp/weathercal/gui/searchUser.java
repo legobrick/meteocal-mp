d535722aeee54a82700c771da601390cf9e4d8a4
Paolo Polidori <polyp91@gmail.com>
2015-02-05 19:26:41 +0100

    tentativo fix tz calendario, roba varia

--- software/WeatherCal/src/main/java/it/polimi/deib/se2/mp/weathercal/gui/searchUser.java
+++ software/WeatherCal/src/main/java/it/polimi/deib/se2/mp/weathercal/gui/searchUser.java
@@ -5,35 +5,28 @@
  */
 package it.polimi.deib.se2.mp.weathercal.gui;
 
-import it.polimi.deib.se2.mp.weathercal.boundary.EventManager;
 import it.polimi.deib.se2.mp.weathercal.boundary.UserManager;
-import it.polimi.deib.se2.mp.weathercal.entity.Event;
 import it.polimi.deib.se2.mp.weathercal.entity.User;
+import it.polimi.deib.se2.mp.weathercal.util.EventConsistencyChecker;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Named;
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ComponentSystemEvent;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import org.primefaces.context.RequestContext;
-import org.primefaces.model.DefaultScheduleModel;
-import org.primefaces.model.ScheduleModel;
 
 /**
  *
@@ -55,7 +48,11 @@ public class searchUser {
 
     private Long calid;
     private String searched;
+    private User reverted;
     
+    public User getReverted(){
+        return reverted;
+    }
 
     public void setSearched(String email) {
         this.searched = email;
@@ -73,23 +70,22 @@ public class searchUser {
         Query q = em.createNamedQuery("User.findByEmail");
         q.setParameter("email", this.searched);
         System.out.println("ricerca");
-        if (q.getResultList().size()>0){
-        User searchus = (User) q.getResultList().get(0);
+        if (q.getResultList().size() > 0) {
+            User searchus = (User) q.getResultList().get(0);
 
-        if (searchus.getCalendarCollection().iterator().next().getIsPublic()) {
+            if (searchus.getCalendarCollection().iterator().next().getIsPublic()) {
 
-            FacesContext.getCurrentInstance().getExternalContext().redirect("searched_user_page.xhtml?id=" + searchus.getCalendarCollection().iterator().next().getId() + "&name=" + searchus.getFirstName() + "&surname=" + searchus.getLastName() + "&mail=" + searchus.getEmail());
-           FacesContext context = FacesContext.getCurrentInstance();
-        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        ServletContext sContext = request.getSession().getServletContext();
-        sContext.setAttribute("selectedEvent",searchus.getCalendarCollection().iterator().next().getId());
+                FacesContext.getCurrentInstance().getExternalContext().redirect("searched_user_page.xhtml?id=" + searchus.getCalendarCollection().iterator().next().getId() + "&name=" + searchus.getFirstName() + "&surname=" + searchus.getLastName() + "&mail=" + searchus.getEmail());
+                FacesContext context = FacesContext.getCurrentInstance();
+                HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+                ServletContext sContext = request.getSession().getServletContext();
+                sContext.setAttribute("selectedEvent", searchus.getCalendarCollection().iterator().next().getId());
+            } else {
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Forbidden", "The user has a private calendar");
+                RequestContext.getCurrentInstance().showMessageInDialog(message);
+            }
         } else {
-            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Forbidden", "The user has a private calendar");
-            RequestContext.getCurrentInstance().showMessageInDialog(message);
-        }
-        }
-        else{
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "User Not Found", "The email insert doesn't match any result");
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "User Not Found", "The email insert doesn't match any result");
             RequestContext.getCurrentInstance().showMessageInDialog(message);
         }
     }
@@ -97,14 +93,33 @@ public class searchUser {
     public void editAction() {
         Map<String, String> params
                 = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-         String action =params.get("action");
+        String action = params.get("action");
         //calid = Long.parseLong(action);
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Forbidden", ""+action);
-                RequestContext.getCurrentInstance().showMessageInDialog(message);
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Forbidden", "" + action);
+        RequestContext.getCurrentInstance().showMessageInDialog(message);
         //  RequestContext.getCurrentInstance().update("schedule1");
-         
+
         RequestContext.getCurrentInstance().update("f:schedule1");
     }
 
-  
+    
+    public void reverse(ComponentSystemEvent event) throws AbortProcessingException {
+        if(FacesContext.getCurrentInstance().isPostback() || searched == null)
+            throwError(FacesContext.getCurrentInstance(), "The value is not a valid User ID:" + searched);
+
+        Query q = em.createNamedQuery("User.findByEmail");
+        q.setParameter("email", searched);
+        if (q.getResultList().isEmpty())
+            throwError(FacesContext.getCurrentInstance(), "The value is not a valid User ID:" + searched);
+        reverted = (User) q.getResultList().get(0);
+    }
+    
+    private void throwError(FacesContext context, String errorText){
+        try {
+            context.getExternalContext().responseSendError(
+                    403, errorText);
+        } catch (IOException ex) {
+            Logger.getLogger(EventConsistencyChecker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
